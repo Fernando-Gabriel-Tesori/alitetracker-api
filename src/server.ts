1ceda6fb37fc0273401fc418a9296e89e9b8606f
Fernando-Gabreil-Tesori <ftesorie123@gmail.com>
2025-05-19 12:39:54 -0300

    modificação

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
- import express, { response } from 'express';
-import { request } from 'http';
+import express, { response } from "express";
 
- const app = express();
+const app = express();
 
- app.get('/', (request, response) => {
-   response.send('Hello Express + TypeScript')
- })
+app.get("/", (request, response) => {
+  response.send("Hello Express + TypeScript");
+});
 
- app.listen(4000, () =>{
-    console.log(`🚀 App is running at port 4000! `)
- })
+app.listen(4000, () => {
+  console.log(`🚀 App is running at port 4000! `);
+});
