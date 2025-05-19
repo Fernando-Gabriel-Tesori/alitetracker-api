2a7676f07ddfab497b4d9e73b2bbb3d5981c192c
Fernando-Gabreil-Tesori <ftesorie123@gmail.com>
2025-05-19 11:27:53 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+ import express, { response } from 'express';
+import { request } from 'http';
+
+ const app = express();
+
+ app.get('/', (request, response) => {
+   response.send('Hello Express + TypeScript')
+ })
+
+ app.listen(4000, () =>{
+    console.log(`🚀 App is running at port 4000! `)
+ })
