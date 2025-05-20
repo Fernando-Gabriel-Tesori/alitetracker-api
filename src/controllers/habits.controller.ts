7ff9e950a622b63bf1ba737fea432cf15f1a955f
Fernando-Gabreil-Tesori <ftesorie123@gmail.com>
2025-05-19 22:35:57 -0300

    modificação

--- src/controllers/habits.controller.ts
+++ src/controllers/habits.controller.ts
@@ -1,24 +1,31 @@
 import type { Request, Response } from "express";
-
-interface Habit {
-  name: string;
-}
+import { habitModel } from "../model/habit.model";
 
 export class HabitsController {
-  private readonly habits: Habit[] = [];
-
-  store = (req: Request, res: Response): Response => {
+  store = async (
+    req: Request,
+    res: Response,
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  ): Promise<Response<any, Record<string, any>>> => {
     const { name } = req.body;
 
+    // Validação básica
     if (!name || typeof name !== "string") {
       return res
         .status(400)
         .json({ error: "O campo 'name' é obrigatório e deve ser uma string." });
     }
 
-    const newHabit: Habit = { name };
-    this.habits.push(newHabit);
+    try {
+      const newHabit = await habitModel.create({
+        name,
+        completedDates: [],
+      });
 
-    return res.status(201).json(newHabit);
+      return res.status(201).json(newHabit);
+    } catch (error) {
+      console.error("Erro ao criar hábito:", error);
+      return res.status(500).json({ error: "Erro interno ao criar o hábito." });
+    }
   };
 }
